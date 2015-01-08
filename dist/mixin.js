9ff49c8c9018974f2d01076020b58740fd91b61e
Jan Nanista <jan.nanista@gooddata.com>
2015-01-08 20:55:18 +0100

    Added build script and first built distribution

---
+++ dist/mixin.js
@@ -0,0 +1,82 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define(factory);
+	else if(typeof exports === 'object')
+		exports["ReactDI"] = factory();
+	else
+		root["ReactDI"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var Mixin = module.exports = function Mixin(map) {
+		this.map = Object.create(map || null);
+	};
+
+	Mixin.prototype.register = function(name, component) {
+		this.map[name] = component;
+	};
+
+	Mixin.prototype.resolve = function(name) {
+		if (name in this.map) {
+			return this.map[name];
+		}
+
+		throw new Error('DI: Component "' + name + '" not found');
+	};
+
+	Mixin.prototype.canResolve = function(name) {
+		return name in this.map;
+	};
+
+
+/***/ }
+/******/ ])
+});
